0597fa0be647172ebcee10015779c37405804785
Felipe <felipecortezfi@gmail.com>
2021-05-31 18:51:49 -0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject weasel "0.7.1"
+(defproject weasel "0.7.2"
   :description "websocket REPL environment for ClojureScript"
   :url "http://github.com/nrepl/weasel"
   :license {:name "Unlicense"
